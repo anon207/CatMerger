79bfeb53956e2e7b4f27be4b9a76b21cc9d2200d
Mark samuel <granbyroll207@gmail.com>
2024-05-01 18:27:21 -0400

    Basic money bar added, components folder added.

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MoneyBar } from './Components/MoneyBar';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MoneyBar />
+      <StatusBar hidden={true} />
     </View>
   );
 }
---
+++ Components/MoneyBar.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export const MoneyBar = ({ money }) => {
+    return(
+        <View style={MoneyBarStyles.container}>
+            <View style={MoneyBarStyles.moneyDisplay}>
+                <Text style={{fontSize: 24}}>money</Text>
+            </View>
+        </View>
+    );
+}
+
+const MoneyBarStyles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '13.5%', 
+        backgroundColor: 'lightgrey',
+        position: 'absolute',
+        top: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    moneyDisplay: {
+        width: '50%',
+        height: '30%',
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
