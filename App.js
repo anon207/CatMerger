61550a8b5785d4758b6e0bf11142691cbf06695a
Mark samuel <granbyroll207@gmail.com>
2024-05-01 21:41:12 -0400

    Trying out some timer stuff

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useState, useEffect } from 'react';
 import { MoneyBar } from './Components/MoneyBar';
 import { UpgradeBar } from './Components/UpgradeBar';
 import { CatContainer } from './Components/CatContainer';
 
 
 export default function App() {
+  const initialTimePerCrate = 5;
+  const [timer, setTimer] = useState(initialTimePerCrate);
+  const [crates, setCrates] = useState(0);
+  const MaxCrates = 5;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(prevTimer => {
+        if (prevTimer <= 0) {
+          return(initialTimePerCrate);
+        }
+        return prevTimer - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MoneyBar />
-      <CatContainer />
+      <MoneyBar timer={timer} />
+      <CatContainer timer={timer} crates={crates} setCrates={setCrates} MaxCrates={MaxCrates}/>
       <UpgradeBar />
       <StatusBar hidden={true} />
     </View>
--- Components/MoneyBar.js
+++ Components/MoneyBar.js
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export const MoneyBar = ({ money }) => {
+export const MoneyBar = ({ timer }) => {
     return(
         <View style={MoneyBarStyles.container}>
             <View style={MoneyBarStyles.moneyDisplay}>
                 <Text style={{fontSize: 24}}>money</Text>
+            </View>
+            <View style={MoneyBarStyles.crateDisplay}>
+                <Text>Time till next crate: {timer}</Text>
             </View>
         </View>
     );
@@ -26,5 +29,11 @@ const MoneyBarStyles = StyleSheet.create({
         backgroundColor: 'red',
         justifyContent: 'center',
         alignItems: 'center',
+        position: 'absolute'
+    },
+    crateDisplay: {
+        position: 'absolute',
+        right: 10,
+        bottom: 5
     },
 });
