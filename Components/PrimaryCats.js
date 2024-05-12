405dab05283183696671d70b8440cbe11ca0be4d
Mark samuel <granbyroll207@gmail.com>
2024-05-11 20:09:28 -0400

    Animations are no longer synced which looks really nice, created new component to reduce clutter in primaryCats.

--- Components/PrimaryCats.js
+++ Components/PrimaryCats.js
@@ -1,101 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { Image, PanResponder, Animated, Dimensions, View, Text } from 'react-native';
+import { PrimaryCatAnimations } from './PrimaryCatAnimations';
 
 export const PrimaryCats = ({ primaryCats, setPrimaryCats }) => {
     const panRespondersRef = useRef([]);
     const { width, height } = Dimensions.get('window');
-
-    useEffect(() => {
-        const spawnIntervals = primaryCats.map(primaryCat => {
-            return setInterval(() => {
-                const spawnedObjectPosition = new Animated.ValueXY({ x: 0, y: 0 });
-
-                Animated.timing(spawnedObjectPosition, {
-                    toValue: { x: 75, y: 15 },
-                    duration: 800,
-                    useNativeDriver: false
-                }).start(() => {
-                    setTimeout(() => {
-                        setPrimaryCats(prevCats => {
-                            const updatedCats = prevCats.map(cat =>
-                                cat.id === primaryCat.id ? { ...cat, spawnedObject: null } : cat
-                            );
-                            return updatedCats;
-                        });
-                        startTextAnimation(primaryCat);
-                    }, 800);
-                });
-
-                const basicYarn = (
-                    <Animated.View
-                        style={{
-                            position: 'absolute',
-                            top: primaryCat.animatedValue.y._value + 12.5,
-                            left: primaryCat.animatedValue.x._value,
-                            transform: [{ translateX: spawnedObjectPosition.x }, { translateY: spawnedObjectPosition.y }]
-                        }}
-                        key={`${primaryCat.id}-${Date.now()}`}
-                    >
-                        <Image
-                            source={require('../assets/redYarn.jpg')}
-                            style={{ width: 25, height: 25 }}
-                        />
-                    </Animated.View>
-                );
-
-                setPrimaryCats(prevCats => {
-                    const updatedCats = prevCats.map(cat =>
-                        cat.id === primaryCat.id ? { ...cat, spawnedObject: basicYarn } : cat
-                    );
-                    return updatedCats;
-                });
-            }, 2000);
-
-        });
-
-        return () => {
-            spawnIntervals.forEach(intervalId => clearInterval(intervalId));
-        };
-    }, [primaryCats, setPrimaryCats]);
-
-    const startTextAnimation = (primaryCat) => {
-        const textPosition = new Animated.ValueXY({ x: 0, y: 0 });
-
-        Animated.timing(textPosition, {
-            toValue: { x: 0, y: -25 },
-            duration: 800,
-            useNativeDriver: false
-        }).start(() => {
-            setPrimaryCats(prevCats => {
-                const updatedCats = prevCats.map(cat =>
-                    cat.id === primaryCat.id ? { ...cat, spawnedObject: null } : cat
-                );
-                return updatedCats;
-            });
-        });
-
-        const plusOne = (
-            <Animated.View
-                style={{
-                    position: 'absolute',
-                    top: primaryCat.animatedValue.y._value,
-                    left: primaryCat.animatedValue.x._value + 75,
-                    transform: [{ translateX: textPosition.x }, { translateY: textPosition.y }]
-                }}
-                key={`${primaryCat.id}-text-${Date.now()}`}
-            >
-                <Text style={{ fontSize: 20 }}>+1</Text>
-            </Animated.View>
-        );
-
-        setPrimaryCats(prevCats => {
-            const updatedCats = prevCats.map(cat =>
-                cat.id === primaryCat.id ? { ...cat, spawnedObject: plusOne } : cat
-            );
-            return updatedCats;
-        });
-    };
-   
 
     useEffect(() => {
         panRespondersRef.current = primaryCats.map(primaryCat => (
@@ -153,7 +62,7 @@ export const PrimaryCats = ({ primaryCats, setPrimaryCats }) => {
                             style={{ width: 50, height: 50 }}
                         />
                     </Animated.View>
-                    {primaryCat.spawnedObject}
+                    <PrimaryCatAnimations primaryCat={primaryCat} setPrimaryCats={setPrimaryCats}/>
                 </View>
             ))}
         </>
