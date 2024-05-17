cb2798c75abc5af1e1e14eeb78bfe0be0c115f4c
Mark samuel <granbyroll207@gmail.com>
2024-05-16 23:45:57 -0400

    Finished up some crate falling and jumping animations, added smoke cloud explosion when crate is opened, there is a bug present where the explosion happens for all cats when a crate is opened, current code is not very organized and needs to be refactored soon.

--- Components/CatContainer.js
+++ Components/CatContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
 import { PrimaryCats } from './PrimaryCats';
 import { CrateAnimations } from './CrateAnimations';
@@ -6,14 +6,19 @@ import { CrateAnimations } from './CrateAnimations';
 export const CatContainer = ({ timer, crates, setCrates, MaxCrates, setMoney, primaryCats, setPrimaryCats }) => {
     const { width, height } = Dimensions.get('window');
     const addedCrateRef = useRef(false);
+    const [showSmoke, setShowSmoke] = useState(false);
+    const [showSmoke2, setShowSmoke2] = useState(false);
+    const [showSmoke3, setShowSmoke3] = useState(false);
+    const [showSmoke4, setShowSmoke4] = useState(false);
+    const [showSmoke5, setShowSmoke5] = useState(false);
 
     useEffect(() => {
         const addCrate = () => {
             if (!addedCrateRef.current && crates.length + primaryCats.length < MaxCrates) {
                 const newCrate = {
                     id: Math.random().toString(),
-                    x: Math.floor(Math.random() * (width * 0.80 - 50)),
-                    y: Math.floor(Math.random() * (height * 0.55 - 50))
+                    x: Math.floor(Math.random() * (width * 0.80 - 75)),
+                    y: Math.floor(Math.random() * (height * 0.55 - 75))
                 };
                 setCrates(prevCrates => [...prevCrates, newCrate]);
                 addedCrateRef.current = true;
@@ -30,9 +35,9 @@ export const CatContainer = ({ timer, crates, setCrates, MaxCrates, setMoney, pr
     return (
         <View style={CatContainerStyles.container}>
             {crates.length > 0 && crates.map(crate => (
-                <CrateAnimations key={crate.id} crate={crate} setCrates={setCrates} setPrimaryCats={setPrimaryCats} />
+                <CrateAnimations key={crate.id} crate={crate} setCrates={setCrates} setPrimaryCats={setPrimaryCats} setShowSmoke={setShowSmoke} setShowSmoke2={setShowSmoke2} setShowSmoke3={setShowSmoke3} setShowSmoke4={setShowSmoke4} setShowSmoke5={setShowSmoke5}/>
             ))}
-            <PrimaryCats primaryCats={primaryCats} setPrimaryCats={setPrimaryCats} setMoney={setMoney} />
+            <PrimaryCats primaryCats={primaryCats} setPrimaryCats={setPrimaryCats} setMoney={setMoney} showSmoke={showSmoke} showSmoke2={showSmoke2} showSmoke3={showSmoke3} showSmoke4={showSmoke4} showSmoke5={showSmoke5}/>
         </View>
     );
 };
--- Components/CrateAnimations.js
+++ Components/CrateAnimations.js
@@ -1,36 +1,128 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Image, Animated, View, Pressable } from 'react-native';
 
-export const CrateAnimations = ({ crate, setCrates, setPrimaryCats }) => {
+export const CrateAnimations = ({ crate, setCrates, setPrimaryCats, setShowSmoke, setShowSmoke2, setShowSmoke3, setShowSmoke4, setShowSmoke5}) => {
+    const OG = useRef(new Animated.ValueXY({ x: crate.x, y: crate.y })).current;
     const position = useRef(new Animated.ValueXY({ x: crate.x, y: -300 })).current;
+    const Xscale = useRef(new Animated.Value(1)).current;
+    const Yscale = useRef(new Animated.Value(1)).current;
 
     const removeCrateAndAddCat = (crate) => {
+        setShowSmoke(true);
+        // setShowSmoke2(true);
+        // setShowSmoke3(true);
+        // setShowSmoke4(true);
+        // setShowSmoke5(true);
+        setTimeout(() => {
+            setShowSmoke(false);
+            setShowSmoke2(true);
+            setShowSmoke3(true);
+            setTimeout(() => {
+                setShowSmoke2(false);
+                setShowSmoke3(false);
+                setShowSmoke4(true);
+                setTimeout(() => {
+                    setShowSmoke4(false);
+                    setShowSmoke5(true);
+                    setTimeout(() => {
+                        setShowSmoke5(false);
+                    }, 160)
+                }, 90)
+            }, 70)
+        }, 40);
+
         setPrimaryCats(prevCats => [...prevCats, { id: crate.id, animatedValue: new Animated.ValueXY({ x: crate.x, y: crate.y }) }]);
         setCrates(prevCrates => prevCrates.filter(oldCrate => oldCrate.id !== crate.id));
     };
 
     useEffect(() => {
-        Animated.timing(position, {
-            toValue: { x: crate.x, y: crate.y },
-            duration: 1000,
-            useNativeDriver: true,
-        }).start();
+        const Xout = Animated.timing(Xscale, { toValue: 1.5, duration: 250, useNativeDriver: true }, position.current = { x: crate.x, y: crate.y });
+        const Xin = Animated.timing(Xscale, { toValue: 1, duration: 250, useNativeDriver: true });
+        const Yout = Animated.timing(Yscale, { toValue: 0.75, duration: 250, useNativeDriver: true });
+        const Yin = Animated.timing(Yscale, { toValue: 1, duration: 250, useNativeDriver: true });
+        const Yup = Animated.sequence([Animated.delay(110), Animated.timing(OG, { toValue: { x: crate.x, y: crate.y - 37.5 }, duration: 250, useNativeDriver: true })]);
+        const YDown = Animated.timing(OG, { toValue: { x: crate.x, y: crate.y }, duration: 250, useNativeDriver: true });
+    
+        const OutStretch = Animated.parallel([Xout, Yout]);
+        const InStretch = Animated.parallel([Xin, Yin]);
+        const UP = Animated.parallel([InStretch, Yup]);
+
+        const loopAnimation = Animated.loop(
+            Animated.sequence([
+                OutStretch,
+                UP,
+                YDown,
+                Animated.delay(4000),
+            ]),
+        );
+    
+        const animateCrate = () => {
+            Animated.sequence([
+                Animated.timing(position, {
+                    toValue: { x: crate.x, y: crate.y },
+                    duration: 500,
+                    useNativeDriver: true,
+                }),
+                Animated.delay(4000),
+                Animated.loop(
+                    Animated.sequence([
+                        OutStretch,
+                        UP,
+                        YDown,
+                        Animated.delay(4000),
+                    ]),
+                ),
+            ]).start();
+        };
+    
+        animateCrate();
+    
+        Animated.sequence([
+            Animated.delay(490),
+            Animated.timing(Xscale, {
+                toValue: 2,
+                duration: 150,
+                useNativeDriver: true
+            }), 
+            Animated.timing(Xscale, {
+                toValue: 1,
+                duration: 150,
+                useNativeDriver: true
+            }), 
+        ]).start();
+    
+        Animated.sequence([
+            Animated.delay(490),
+            Animated.timing(Yscale, {
+                toValue: .7,
+                duration: 150,
+                useNativeDriver: true
+            }), 
+            Animated.timing(Yscale, {
+                toValue: 1,
+                duration: 150,
+                useNativeDriver: true
+            }), 
+        ]).start();
     }, [crate]);
 
     return (
-        <Animated.View
-            style={{
-                position: 'absolute',
-                left: crate.x,
-                transform: [{ translateY: position.y }],
-            }}
-        >
-            <Pressable onPress={() => removeCrateAndAddCat(crate)}>
-                <Image
-                    source={require('../assets/Crates/GreenWhiteCrate.png')}
-                    style={{ width: 50, height: 50 }}
-                />
-            </Pressable>
-        </Animated.View>
+            <Animated.View
+                style={{
+                    position: 'absolute',
+                    left: crate.x,
+                    transform: [
+                        { translateY: position.y._value === crate.y ? OG.y : position.y },
+                        { scaleX: Xscale },
+                        { scaleY: Yscale }],
+                }}
+            >
+                <Pressable onPress={() => removeCrateAndAddCat(crate)}>
+                    <Image
+                        source={require('../assets/Crates/GreenWhiteCrate.png')}
+                        style={{ width: 75, height: 75 }}
+                    />
+                </Pressable>
+            </Animated.View>
     );
 };
